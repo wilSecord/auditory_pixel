f81e57be7a694c46d99ad41dcdaf5b6b3463b5d2
Wil <wilsonsecord@gmail.com>
2020-08-17 21:10:56 -0400

    finished (for the most part) pixel obtaining of the project

--- sound/pixel.py
+++ sound/pixel.py
@@ -1,7 +1,8 @@
 import pyautogui as pag
 
-sc = pag.screenshot()
-px = sc.getpixel(xy=(960, 540))
-sound = (px[0] + px[1] + px[2])/3
+while True:
+    sc = pag.screenshot()
+    px = sc.getpixel(xy=(960, 540))
+    sound = (px[0] + px[1] + px[2])/3
 
-print(sound)
+    print(sound*4)
